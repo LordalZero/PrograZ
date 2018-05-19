5429f4b1b4bd34e1081917ca736819e58691d49e
Henson <henson-12@hotmail.com>
2018-05-18 21:50:37 -0500

    agregando clase Enemigos
    he agregado la clase Enemigos

---
+++ PrograZ/Enemigos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Enemigos : Entidades 
+{
+    public int ataque;
+    public int vida;
+    public int valor;
+    public int rapidez;
+
+    public Enemigos(int atk, int vid, int val, int rap)
+    {
+        ataque = atk;
+        vida = vid;
+        valor = val;
+        rapidez = rap;
+    }
+
+    public int Ataque { get; set; }
+    public int Vida { get; set; }
+    public int Rapidez { get; set; }
+    public int Valor { get; set; }
+
+	public void golpear()
+	{
+		
+	}
+}
