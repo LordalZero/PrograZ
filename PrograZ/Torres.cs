760bb3f59421ae255f32c28bfc5555d5bd41f706
Tobias <victorandrebravoburga@gmail.com>
2018-05-18 21:48:37 -0500

    Torres
    Agregando Clase Torres

---
+++ PrograZ/Torres.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class PrograZ
+{
+    class Torres
+    {
+        public int velocidad { get; set; }
+        public int costo { get; set; }
+        public int nivel { get; set; }
+        public int vida { get; set; }
+
+    }
+
+
+
+    public void Mejorar( int nivel, int costo)
+    {
+
+    }
+
+    public void Destruir()
+    {
+
+    }
+
+
+
+}
