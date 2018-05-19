1f75eb1bfef186dcce0d987c53934e2c88238c87
LordalZero <lordalzero@gmail.com>
2018-05-18 21:34:42 -0500

    Agregado 2 clases
    - Agregado Clase Entidades y sus variables
    - Agregado Clase Jugador y sus variables

---
+++ PrograZ/Jugador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograZ
+{
+    class Jugador
+    {
+        public int hp { get; set; }
+        public int dinero { get; set; }
+        public int puntaje { get; set; }
+    }
+}
