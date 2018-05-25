118f327c0d394f156a304d708d73f46ca5d4e615
Henson Jimenez <henson-12@hotmail.com>
2018-05-25 10:41:51 -0500

    Mejorado el script Enemigos y agregado el script Aereos

---
+++ PrograZ/Aereos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrograZ
+{
+    public class Aereos : Enemigos
+    {
+        public Aereos(int atk, int vid, int val, int rap) : base(atk, vid, val, rap)
+        {
+
+        }
+       
+        public void Volar()
+        {
+            
+        }
+    }
+
+}
+
--- PrograZ/Enemigos.cs
+++ PrograZ/Enemigos.cs
@@ -3,28 +3,53 @@
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-public class Enemigos : Entidades 
+namespace PrograZ
 {
-    public int ataque;
-    public int vida;
-    public int valor;
-    public int rapidez;
-
-    public Enemigos(int atk, int vid, int val, int rap)
+    public class Enemigos : Entidades
     {
-        ataque = atk;
-        vida = vid;
-        valor = val;
-        rapidez = rap;
+        public int ataque;
+        public int vida;
+        public int valor;
+        public int rapidez;
+
+        public Enemigos(int atk, int vid, int val, int rap)
+        {
+            ataque = atk;
+            vida = vid;
+            valor = val;
+            rapidez = rap;
+        }
+
+        public int Ataque
+        {
+            get { return ataque; }
+            set { ataque = value; }
+        }
+
+        public int Vida
+        {
+            get { return vida; }
+            set { vida = value; }
+        }
+
+        public int Rapidez
+        {
+            get { return rapidez; }
+            set { rapidez = value; }
+        }
+
+        public int Valor
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+
+        public void Golpear()
+        {
+            ataque = 0;
+        }
     }
 
-    public int Ataque { get; set; }
-    public int Vida { get; set; }
-    public int Rapidez { get; set; }
-    public int Valor { get; set; }
-
-	public void golpear()
-	{
-		
-	}
 }
+
+   
