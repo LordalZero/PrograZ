7571b4d962e399d1eb488dba3abd3c809c9c0e60
Victor Andre Bravo Burga <victorandrebravoburga@gmail.com>
2018-05-25 21:25:21 -0500

    Agregando Clase Danio

---
+++ PrograZ/Danio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Practica
+{
+    public class Danio : Torres
+    {
+
+        public int damage;
+
+
+
+        public Danio(int vel, int cos, int niv, int vid, int dam) : base(vel, cos, niv, vid, dam)
+        {
+
+            damage = dam;
+
+        }
+
+        public void Atacar()
+        {
+
+        }
+
+    }
+}
