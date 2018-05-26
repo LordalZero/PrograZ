ecc1345fb3a1156907a0c5514f692439e392dbb6
Victor Andre Bravo Burga <victorandrebravoburga@gmail.com>
2018-05-25 21:38:52 -0500

    Agregando Clase Soporte

---
+++ PrograZ/Soporte.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Soporte : Torres
+{
+
+    public float ralentizacion;
+    public float area;
+
+
+
+    public Soporte(int vel, int cos, int niv, int vid, float ral, float ar) : base(vel, cos, niv, vid, ral, ar)
+    {
+
+        ralentizacion = ral;
+        area = ar;
+
+    }
+
+    public void DanioArea()
+    {
+
+    }
+
+}
