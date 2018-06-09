f0276774eb68db6cd5a1d8c58ecf1a8d5188d4b8
Henson Jimenez <henson-12@hotmail.com>
2018-06-08 21:54:26 -0500

    Agregado el script Inputsplayer

---
+++ ProyectoUnity/Assets/Scripts/Controller/Inputsplayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PrograZ;
+
+public class Inputsplayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.E))
+            Debug.Log("Torre Destruida");
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            Debug.Log("Torre Colocada");
+		
+	}
+}
