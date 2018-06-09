4996c341c07b7c187a5af387d7eee1f59ffd0196
Rafael Condor <rafincr23@gmail.com>
2018-06-08 21:55:28 -0500

    Agregando vista al enemigo

---
+++ ProyectoUnity/Assets/Scripts/View/Vista_Enemigo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PrograZ;
+
+public class Vista_Enemigo : MonoBehaviour {
+
+    Enemigos enemigos = new Enemigos(10, 5, 5, 80);
+
+    // Use this for initialization
+    void Start () {
+
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (enemigos.vida < 1)                                                              //Voy a poner una resta de la vida al llegar a cero se destruye
+        {
+          
+            
+            Debug.Log("se destruye este enemigo");     
+        }
+	}
+
+
+}
