d5c500a2353dcbd3a022cc854d14e2fa93ecad9f
Henson Jimenez <henson-12@hotmail.com>
2018-06-22 12:39:52 -0500

    Agregados los scripts EnemigoAereo, EnemigoPesado y EnemigoRapido

---
+++ ProyectoUnity/Assets/Scripts/Model/EnemigoAereo.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PrograZ;
+
+public class EnemigoAereo {
+
+	public int ataque;
+	public int vida;
+	public int valor;
+	public int rapidez;
+
+
+	public EnemigoAereo(int atk, int vid, int val, int rap){
+		ataque = atk;
+		vida = vid;
+		valor = val;
+		rapidez = rap;
+	}
+
+	public int Ataque
+	{
+		get
+		{
+			return ataque;
+		}
+		set
+		{
+			ataque = value;
+		}
+	}
+
+	public int Vida
+	{
+		get
+		{
+			return vida;
+		}
+		set
+		{
+			vida = value;
+		}
+	}
+
+	public int Valor
+	{
+		get
+		{
+			return valor;
+		}
+		set
+		{
+			valor = value; 
+		}
+	}
+
+	public int Rapidez
+	{
+		get
+		{
+			return rapidez;
+		}
+		set
+		{
+			rapidez = value;
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
