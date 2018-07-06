6a3a506116b0f182fbe12c27fc37c1abe741c6aa
LordalZero <lordalzero@gmail.com>
2018-07-06 02:36:34 -0500

    Agregado Logica de Asignacion de Torres
    - Agregado Selector de casillas para torres
    - Agregado asignacion de torres a la casilla seleccionada

---
+++ ProyectoUnity/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    void Update()
+    {
+        //if (player.health < 1)
+       // {
+        //    EndGameCondition();
+       // }
+    }
+
+    void EndGameCondition ()
+    {
+        // Reservado para view (Pantalla de puntaje final con score)
+    }
+}
--- ProyectoUnity/Assets/Scripts/Controller/TowerController.cs
+++ ProyectoUnity/Assets/Scripts/Controller/TowerController.cs
@@ -7,19 +7,25 @@ public class TowerController : MonoBehaviour {
     public int towerType;
     public int towerIndex;
     public string towerSelection;
-    public List<TorreBasica> listaDeTorresBasicas;
+    //public List<TorreBasica> listaDeTorresBasicas;
+    public TorreBasica[] torres;
+    public GameObject[] torresObjeto;
+    public GameObject torreBasicaObjeto;
+    public GameObject canvasGameObj;
+
 
 
 
 	void Start () {
+        torres = new TorreBasica[8];
         towerType = 0;
         towerIndex = 0;
         towerSelection = "";
-        listaDeTorresBasicas = new List<TorreBasica>();
+        //listaDeTorresBasicas = new List<TorreBasica>();
 	}
 	
 
-	public void CrearTorrePrueba() {
+	public void CrearTorrePrueba() { // Debuggin Purposes
 
         TorreBasica torrecitaZero = new TorreBasica(79, 79, 79, 79);
         Debug.Log(torrecitaZero.velocidad);
@@ -28,7 +34,8 @@ public class TowerController : MonoBehaviour {
 
     public void AssignBasicTower2Selection ()
     {
-        listaDeTorresBasicas.Insert(towerIndex, new TorreBasica(10, 10, 10, 10));
+        //listaDeTorresBasicas.Insert(towerIndex, new TorreBasica(10, 10, 10, 10));
+        torres[towerIndex] = new TorreBasica(5,3,1,4);
     }
 
     public void AssignIndexTower (int towerSelection)
@@ -38,7 +45,25 @@ public class TowerController : MonoBehaviour {
 
     public void ShowTowerSelectedValues ()
     {
-        Debug.Log("Torre Numero "+ towerIndex +" Velocidad: " + listaDeTorresBasicas[towerIndex].velocidad + " Costo: " + listaDeTorresBasicas[towerIndex].costo + " Nivel: " + listaDeTorresBasicas[towerIndex].nivel + " Ataque: " + listaDeTorresBasicas[towerIndex].danio);
+        Debug.Log("Torre Numero "+ towerIndex +" Velocidad: " + torres[towerIndex].velocidad + " Costo: " + torres[towerIndex].costo + " Nivel: " + torres[towerIndex].nivel + " Ataque: " + torres[towerIndex].danio);
+    }
+
+    public void UnassignTower ()
+    {
+
+    }
+
+    public void TowerCheck ()
+    {
+        for (int i = 0; i <= torres.Length - 1; i++)
+        {
+            if (torres[i] != null)
+            {
+                GameObject nuevaTorreBasica = Instantiate(torreBasicaObjeto, torresObjeto[towerIndex].transform.position, torresObjeto[towerIndex].transform.rotation) as GameObject;
+                nuevaTorreBasica.transform.parent = canvasGameObj.transform;
+                Debug.Log("Setting Tower to Slot: " + towerIndex);
+            }
+        }
     }
 
     void Update()
