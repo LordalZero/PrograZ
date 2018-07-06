b5706be8103de66d77f0682a3dbbaf4588ed2af6
Henson Jimenez <henson-12@hotmail.com>
2018-07-06 09:30:16 -0500

    Agregado el script Player

---
+++ ProyectoUnity/Assets/Scripts/Model/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PrograZ;
+
+public class Player : Entidades {
+
+	public int vida;
+
+
+
+
+	public Player(int hp){
+		
+		vida = hp;
+		
+	}
+
+	public Player myPlayer = new Player (50);
+
+	public int Vida
+	{
+		get
+		{
+			return vida;
+		}
+		set
+		{
+			vida = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
